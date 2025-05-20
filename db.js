8ee6e4e979ee118557302fea14b92bea5847b083
Josue <memejosue2012@hotmail.com>
2025-05-19 22:58:50 -0600

    Modificado backend/db.js

--- db.js
+++ db.js
@@ -4,8 +4,8 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: '12345',
-  database: 'bdcine',
+  password: '',
+  database: 'cine',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
